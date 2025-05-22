efe14cc7817527052d68b738a7f0a4ea52ee021f
Polina Boeva <polina.boeva.2003@mail.ru>
2025-05-22 12:54:03 +0200

    список диалогов. опять

---
+++ app/src/main/java/com/diplom/rande_vuz/models/Chat.kt
@@ -0,0 +1,9 @@
+package com.diplom.rande_vuz.models
+
+data class Chat(
+    val id: String = "",
+    val userIds: List<String> = listOf(),
+    val lastMessage: String = "",
+    val timestamp: Long = 0,
+    val chatName: String = ""
+)
